cfee45b5ed6b6aced0d5b42b07995fc6728acecc
sobanjawaid26 <sobanjawaid26@gmail.com>
2023-11-20 18:31:55 +0530

    Initial

---
+++ src/main/java/com/sobanscode/App.java
@@ -0,0 +1,13 @@
+package com.sobanscode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
